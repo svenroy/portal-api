aa5daab9f0d3772fa9b4323ff4b6241383c90891
Royston Yinkore <roystonyinkore@gmail.com>
2018-05-28 14:16:06 +0100

    Added swagger

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import morgan from 'morgan';
 import express from 'express';
 import app from './api';
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+
 const debug = require('debug')('app');
 
 const port = process.env.PORT || 3000;
@@ -10,6 +13,8 @@ const port = process.env.PORT || 3000;
 const server = express();
 
 server.use(morgan('tiny'));
+
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.configureControllers(server);
 
 server.listen(port, () => {
